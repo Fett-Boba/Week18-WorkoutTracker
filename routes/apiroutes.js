f3b525a58f7350248e76accda06d1dd95f0c7b33
Boba Fett <thugfencer@gmail.com>
2021-08-12 00:46:06 -0700

    Get workout put and post routes working

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,43 +1,61 @@
-//const path = require('path');
 const Workout = require('../models/Workout.js');
 const router = require('express').Router();
 
-// const db = require("../models");
-//const { Workout } = require("./models");
-
-// Route for Dashboard
+// Route for Dashboard - only display 7 days
 router.get("/api/workouts/range", (req, res) => {
      Workout.aggregate([
           { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
-     ]).then(dbWorkout => {
-          res.json(dbWorkout);
-     }).catch(err => {
-          console.log(err);
-          res.sendStatus(500);
-     })
+     ])
+          // .sort({ $natural: 1 }).limit(7)
+          .sort({ day: -1 }).limit(7)
+          .then(dbWorkout => {
+               res.json(dbWorkout);
+          }).catch(err => {
+               console.log(err);
+               res.sendStatus(500);
+          })
 });
 
-// Route to find the last workout done
+// Route to find the last workout stats
 router.get("/api/workouts", (req, res) => {
      Workout.aggregate([
           { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
-     ]).then(dbWorkout => {
-          res.json(dbWorkout);
-     }).catch(err => {
-          console.log(err);
-          res.sendStatus(500);
-     })
+     ])
+          .sort({ day: -1 }).limit(1)
+          .then(dbWorkout => {
+               res.json(dbWorkout);
+          }).catch(err => {
+               console.log(err);
+               res.sendStatus(500);
+          })
 });
 
-// Route to add new exercise
+// Route to add a new workout
 router.post("/api/workouts", (req, res) => {
-     Workout.create(req.body)
+     Workout.create({})
           .then((dbWorkout) => {
                res.json(dbWorkout);
           })
           .catch((err) => {
                res.json(err);
           });
+});
+
+// Route to add an exercise to existing workout
+router.put("/api/workouts/:id", (req, res) => {
+     Workout.updateOne(
+          { _id: req.params.id },
+          { $push: { exercises: req.body } },
+          (err, dbWorkout) => {
+               if (err) {
+                    console.log(err);
+                    res.send(err);
+               } else {
+                    console.log(dbWorkout);
+                    res.send(dbWorkout);
+               }
+          }
+     );
 });
 
 module.exports = router;
