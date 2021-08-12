e442060daf000b587e598d18d566dc15cee321ff
Boba Fett <thugfencer@gmail.com>
2021-08-12 12:25:17 -0700

    Complete last two routes, code cleanup

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,7 +6,7 @@ router.get("/api/workouts/range", (req, res) => {
      Workout.aggregate([
           { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
      ])
-          // .sort({ $natural: 1 }).limit(7)
+          // .sort({ $natural: -1 }).limit(7)
           .sort({ day: -1 }).limit(7)
           .then(dbWorkout => {
                res.json(dbWorkout);
--- server.js
+++ server.js
@@ -2,19 +2,12 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
-
-//const db = require("./models");
-//const { Workout } = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
