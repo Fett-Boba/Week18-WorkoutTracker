68bef524b36560f01f910d2fbe483e288adb22e3
Boba Fett <thugfencer@gmail.com>
2021-08-11 22:04:23 -0700

    Create route to display the last workout (main page)

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const db = require("../models");
 //const { Workout } = require("./models");
 
-
+// Route for Workout Dashboard
 router.get("/api/workouts/range", (req, res) => {
      db.Workout.find({}, (err, data) => {
           if (err) {
@@ -14,6 +14,18 @@ router.get("/api/workouts/range", (req, res) => {
                res.json(data);
           }
      }).sort({ $natural: -1 }).limit(7);
+});
+
+// Route to find the last workout done
+router.get("/api/workouts", (req, res) => {
+     db.Workout.find({}, (err, data) => {
+          if (err) {
+               console.log(err);
+               res.sendStatus(500);
+          } else {
+               res.json(data);
+          }
+     }).sort({ $natural: -1 }).limit(1);
 });
 
 
